1898354b2a625bbcd1f09e50bb34383d22ae59b4
Evan Sonderegger <evan.sonderegger@gmail.com>
2016-10-14 11:38:06 -0400

    remove unused import

--- appsrc/js/podcast-settings.js
+++ appsrc/js/podcast-settings.js
@@ -4,7 +4,6 @@ import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
-// import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
 
 export default class PodcastSettings extends React.Component {
