5296407a89449614b7d32c0ca5945f76d1693342
Evan Sonderegger <evan.sonderegger@gmail.com>
2016-10-16 13:33:14 -0400

    perform 2-pass encoding normalizing audio to -18 LUFS

--- appsrc/js/encode-audio.js
+++ appsrc/js/encode-audio.js
@@ -3,14 +3,46 @@ const fs = require('fs');
 const path = require('path');
 
 function ffmpegPath() {
-  return path.join(__dirname, 'compiled', 'ffmpeg_mac');
+  return path.join(__dirname, 'compiled', 'ffmpeg_mac2');
 }
 
-export default function encodeAudio(srcPath, destDir, handleChange) {
-  var outPath = path.join(destDir, 'media', path.parse(srcPath).name + '.mp3');
+function measureLoudness(srcPath, destDir, handleChange) {
   var opts = [
     '-i',
     srcPath,
+    '-af',
+    'loudnorm=I=-18:TP=-0.5:LRA=9:print_format=json',
+    '-f',
+    'null',
+    '-'
+  ];
+  var stderrText = '';
+  var ff = childProcess.spawn(ffmpegPath(), opts);
+  ff.stderr.on('data', function(data) {
+    stderrText += data;
+  });
+  ff.on('exit', (code, signal) => {
+    var jsonStartIndex = stderrText.lastIndexOf('{');
+    var jsonString = stderrText.slice(jsonStartIndex);
+    var measuredJson = JSON.parse(jsonString);
+    console.log(measuredJson);
+    secondPass(srcPath, destDir, measuredJson, handleChange);
+  });
+}
+
+function secondPass(srcPath, destDir, loudnessInfo, handleChange) {
+  var outPath = path.join(destDir, 'media', path.parse(srcPath).name + '.mp3');
+  var afString = 'measured_I=' + loudnessInfo.input_i +
+    ':measured_LRA=' + loudnessInfo.input_lra +
+    ':measured_TP=' + loudnessInfo.input_tp +
+    ':measured_thresh=' + loudnessInfo.input_thresh +
+    ':offset=' + loudnessInfo.target_offset +
+    ':linear=true:print_format=summary';
+  var opts = [
+    '-i',
+    srcPath,
+    '-af',
+    'loudnorm=I=-18:TP=-0.5:LRA=9:' + afString,
     '-codec:a',
     'libmp3lame',
     '-b:a',
@@ -24,4 +56,8 @@ export default function encodeAudio(srcPath, destDir, handleChange) {
     handleChange('filesize', stats.size);
     handleChange('filetype', 'audio/mpeg');
   });
+}
+
+export default function encodeAudio(srcPath, destDir, handleChange) {
+  measureLoudness(srcPath, destDir, handleChange);
 }
